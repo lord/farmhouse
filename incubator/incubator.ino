03535fe4cad57f501cda5e0e3fbf21a3e90a2baa
Robert Lord <robert@lord.io>
2016-11-13 03:58:54 -0600

    Add sous vide cooker

--- incubator/incubator.ino
+++ incubator/incubator.ino
@@ -1,6 +1,12 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #include "DHT.h"
 #define DHTPIN 2        // digital pin of temp sensor
 #define DHTTYPE DHT11   // DHT 11
+#define ONE_WIRE_BUS 3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature tempSensor(&oneWire);
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -18,9 +24,21 @@ int errorPin = 13;
 float incubatorLow = 95.0; // below this temp the heater/lights activate
 float incubatorHigh = 98.0; // above this temp the heater/lights deactivate
 
+// cooker temps are in celsius
+
+// best eggs
+// float cookerLow = 62.5; // 144.5 F
+// float cookerHigh = 62.8; // 145.0 F
+// see http://www.seriouseats.com/2013/10/sous-vide-101-all-about-eggs.html
+
+// garlic stuff
+float cookerLow = 88.0;
+float cookerHigh = 88.5;
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
+  tempSensor.begin();
   pinMode(incubatorPin, OUTPUT);
   pinMode(cookerPin, OUTPUT);
   pinMode(errorPin, OUTPUT);
@@ -42,12 +60,30 @@ void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
 
-  bool ok = incubatorLoop();
+  bool ok = true;
+  ok = incubatorLoop() && ok;
+  ok = cookerLoop() && ok;
   if (!ok) {
     errorBlink(3, 100);
     errorBlink(3, 200);
     errorBlink(3, 100);
   }
+}
+
+bool cookerLoop() {
+  Serial.print("Updating liquid temperature sensor...");
+  tempSensor.requestTemperatures();
+  int t = tempSensor.getTempCByIndex(0);
+  Serial.print(t);
+  if (t < cookerLow) {
+    digitalWrite(cookerPin, LOW);
+    Serial.print(", so we're below cooker threshold");
+  } else if (t > cookerHigh) {
+    digitalWrite(cookerPin, HIGH);
+    Serial.print(", so we're above cooker threshold");
+  }
+  Serial.print("\n");
+  return true;
 }
 
 bool incubatorLoop() {
